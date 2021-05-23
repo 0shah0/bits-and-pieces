b197843fe1afff4e26c1455f9a7b179979fad193
0shah0 <84684114+0shah0@users.noreply.github.com>
2021-05-24 01:41:38 +0530

    Create divide-and-conquer-find-a-value-n-an-array.js
    Recursive and divide & conquer approach to find a value in array

---
+++ divide-and-conquer-find-a-value-n-an-array.js
@@ -0,0 +1,13 @@
+let findaValue = (hs, n)=>{
+   if(hs.length == 1)
+   return (hs[0] == n);
+   
+   let half = Math.floor(hs.length/2);
+   let left = hs.slice(0, half);
+   let right = hs.slice(half);
+   let ph = (left[left.length-1] >= n ) ? left : right;
+   return findValue(ph, n);
+}
+
+findValue([1,2,3,7,8,9,11,12,15,30], 15) // true
+findValue([1,2,3,7,8,9,11,12,15,30], 13) // false
